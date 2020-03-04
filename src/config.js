04890c07738f4d1217bea7e308cdaf3da50f4853
Perla Del Castillo <perlitadelcastillo@gmail.com>
2020-03-03 20:27:42 -0500

    Updated file to not have database details

--- src/config.js
+++ src/config.js
@@ -3,14 +3,14 @@ import '@react-native-firebase/database';
 import '@react-native-firebase/firestore';// needed
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCtmsSdW6L7swIhtVlhpha25HOk5R_6F18",
-    authDomain: "study-spot-finder-58b35.firebaseapp.com",
-    databaseURL: "https://study-spot-finder-58b35.firebaseio.com",
-    projectId: "study-spot-finder-58b35",
-    storageBucket: "study-spot-finder-58b35.appspot.com",
-    messagingSenderId: "144048725017",
-    appId: "1:144048725017:web:2db24b9f9a3b0288b99e88",
-    measurementId: "G-8L993Q1E50"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   //let app =  firebase.initializeApp(firebaseConfig);
